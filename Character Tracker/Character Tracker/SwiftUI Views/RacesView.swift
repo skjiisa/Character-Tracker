4a33031bd30220fcbbc243fd7e05f9373a6f1be3
Isvvc <lyons@tuta.io>
2020-09-02 20:43:19 -0600

    Allow Races from other games to be added

--- Character Tracker/Character Tracker/SwiftUI Views/RacesView.swift
+++ Character Tracker/Character Tracker/SwiftUI Views/RacesView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct RacesView: View {
+    @Environment(\.managedObjectContext) var moc
     var fetchRequest: FetchRequest<Race>
     var races: FetchedResults<Race> {
         fetchRequest.wrappedValue
@@ -16,16 +17,38 @@ struct RacesView: View {
     
     @EnvironmentObject var gameReference: GameReference
     
+    @State private var showingAddRaceSheet = false
+    @State private var showingNewRaceAlert = false
+    @State private var showingAllRaces = false
+    
+    var game: Game?
     var didSelect: (Race) -> Void
+    var excluding: Bool
     
     init(game: Game?, didSelect: @escaping (Race) -> Void = {_ in}) {
         var predicate: NSPredicate?
         if let game = game {
             predicate = NSPredicate(format: "%@ in games", game)
         }
-        fetchRequest = FetchRequest(entity: Race.entity(), sortDescriptors: [NSSortDescriptor(key: "name", ascending: false)], predicate: predicate)
+        fetchRequest = FetchRequest(entity: Race.entity(), sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)], predicate: predicate)
         
+        self.game = game
         self.didSelect = didSelect
+        self.excluding = false
+    }
+    
+    init(excluding game: Game?, didSelect: @escaping (Race) -> Void = {_ in}) {
+        var predicate: NSPredicate?
+        if let races = game?.races {
+            // I don't know why it has to be done this way, but just inverting the
+            // predicate from the other initializer results in weird behavior.
+            predicate = NSPredicate(format: "NOT SELF in %@", races)
+        }
+        fetchRequest = FetchRequest(entity: Race.entity(), sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)], predicate: predicate)
+        
+        self.game = game
+        self.didSelect = didSelect
+        self.excluding = true
     }
     
     var body: some View {
@@ -34,7 +57,44 @@ struct RacesView: View {
                 Button(race.name ?? "Unknown race") {
                     self.didSelect(race)
                 }
+                .foregroundColor(.primary)
             }
+            
+            if !excluding {
+                Section {
+                    Button(action: {
+                        self.showingAddRaceSheet = true
+                    }) {
+                        HStack {
+                            Spacer()
+                            Text("Add Race")
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("Races")
+        .actionSheet(isPresented: $showingAddRaceSheet) {
+            ActionSheet(title: Text("Add Race"), buttons: [
+                .default(Text("Create new race"), action: {
+                    self.showingNewRaceAlert = true
+                }),
+                .default(Text("Add race from other game"), action: {
+                    self.showingAllRaces = true
+                }),
+                .cancel()
+            ])
+        }
+        .sheet(isPresented: $showingAllRaces) {
+            NavigationView {
+                RacesView(excluding: self.game) { race in
+                    guard let game = self.game else { return }
+                    race.mutableSetValue(forKey: "games").add(game)
+                    // Save
+                }
+            }
+            .environment(\.managedObjectContext, self.moc)
         }
     }
 }
