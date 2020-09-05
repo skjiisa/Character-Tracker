c56c042770976f1e41930fe0cb8be29a48c7ba9c
Isvvc <lyons@tuta.io>
2020-09-04 20:58:45 -0600

    Refactor collapsible sections to not change title label

--- Character Tracker/Character Tracker/SwiftUI Views/CharacterDetailView.swift
+++ Character Tracker/Character Tracker/SwiftUI Views/CharacterDetailView.swift
@@ -28,30 +28,25 @@ struct CharacterDetailView: View {
         }
     }
     
-    func section<Content: View>(_ section: TempSection, content: () -> Content) -> some View {
-        Section(header:
-            Button(action: {
-                self.sectionController.toggleSection(section, for: self.character)
-            }, label: {
-                Text((section.collapsed ? "▶︎" : "▼") + "\t" + section.name.uppercased())
-                // Spacer fills the width, allowing the empty space after to be tapped too.
-                Spacer()
-            })
-                .foregroundColor(.secondary)
-        ) {
-            content()
-        }
-    }
-    
-    func collapsibleSection<EditModeContent: View, SectionContent: View>(_ section: TempSection, editModeSectionContent: EditModeContent, content: () -> SectionContent) -> some View {
+    func collapsibleSection<EditModeContent: View, SectionContent: View>(_ section: TempSection, editModeSectionContent: () -> EditModeContent, content: () -> SectionContent) -> some View {
         Group {
-            if !section.collapsed {
-                self.section(section, content: content)
-                if self.editMode {
-                    editModeSectionContent
+            Section(header:
+                Button(action: {
+                    self.sectionController.toggleSection(section, for: self.character)
+                }, label: {
+                    Text((section.collapsed ? "▶︎" : "▼") + "\t" + section.name.uppercased())
+                    // Spacer fills the width, allowing the empty space after to be tapped too.
+                    Spacer()
+                })
+                    .foregroundColor(.secondary)
+            ) {
+                if !section.collapsed {
+                    content()
                 }
-            } else {
-                self.section(section) { EmptyView() }
+            }
+            
+            if editMode && !section.collapsed {
+                Section(content: editModeSectionContent)
             }
         }
     }
@@ -85,15 +80,17 @@ struct CharacterDetailView: View {
             ForEach(sectionController.tempSections(for: character), id: \.self) { section in
                 Group {
                     if section.section is ModuleType {
-                        self.collapsibleSection(section, editModeSectionContent:
+                        self.collapsibleSection(section, editModeSectionContent: {
                             NavigationLink("Add new \(section.name)", destination: ModulesView(character: self.character, didSelect: { module in
                                 print(module.name)
                             }))
-                        ) {
+                        }) {
                             CharacterModuleSection(section.section as! ModuleType, character: self.character)
                         }
                     } else if section.section is AttributeTypeSection {
-                        self.collapsibleSection(section, editModeSectionContent: Text("Add new \(section.name)")) {
+                        self.collapsibleSection(section, editModeSectionContent: {
+                            Text("Add new \(section.name)")
+                        }) {
                             CharacterAttributeSection(section.section as! AttributeTypeSection, character: self.character)
                         }
                     }
